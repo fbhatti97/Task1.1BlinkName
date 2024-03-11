af18f2740c6d0470f62874cc6a17653ab3b9c3a9
fbhatti97 <141708863+fbhatti97@users.noreply.github.com>
2024-03-11 14:32:29 +1100

    Update task_1_11.ino

--- task_1_11.ino
+++ task_1_11.ino
@@ -5,20 +5,19 @@ const int ledPin = 13;
 // Morse code timings
 const int dotTime = 300;
 const int dashTime = 800;
-const int partGap = 300; // Gap between dots and dashes of the same letter
-const int letterGap = 900; // Gap between letters
-const int wordGap = 1400; // Gap between words
+const int partGap = 350; // Gap between dots and dashes of the same letter
+const int letterGap = 900; 
+const int wordGap = 1400; 
 
-// Variable to keep track of the button press state
+// Keep track of the button press state
 bool isButtonPressed = false;
 
-// Setup function to initialize the pin modes
+
 void setup() {
   pinMode(buttonPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
-// Function to blink a dot
 void blinkDot() {
   digitalWrite(ledPin, HIGH);
   delay(dotTime);
@@ -26,7 +25,6 @@ void blinkDot() {
   delay(partGap);
 }
 
-// Function to blink a dash
 void blinkDash() {
   digitalWrite(ledPin, HIGH);
   delay(dashTime);
@@ -34,7 +32,6 @@ void blinkDash() {
   delay(partGap);
 }
 
-// Function to spell a letter in Morse code
 void spellLetter(char letter) {
   switch (letter) {
     case 'F':
@@ -52,18 +49,17 @@ void spellLetter(char letter) {
     case 'L':
       blinkDot(); blinkDash(); blinkDot(); blinkDot();
       break;
-    // Add more cases for other letters as needed
+    
   }
-  delay(letterGap); // Gap between letters
+  delay(letterGap); 
 }
 
-// Loop function that waits for a button press to start the blinking
-void loop() {
-  // Check if the button is pressed
-  if (digitalRead(buttonPin) == HIGH && !isButtonPressed) {
-    isButtonPressed = true; // Mark the button as pressed
 
-    // Spell out the name
+void loop() {
+  
+  if (digitalRead(buttonPin) == HIGH && !isButtonPressed) {
+    isButtonPressed = true; 
+
     spellLetter('F');
     spellLetter('A');
     spellLetter('Y');
@@ -71,9 +67,9 @@ void loop() {
     spellLetter('A');
     spellLetter('L');
 
-    // After spelling the name, wait for the button to be released
+
     while(digitalRead(buttonPin) == HIGH) {
-      // Do nothing, just wait
+
     }
     isButtonPressed = false; // Reset the button press state
   }
